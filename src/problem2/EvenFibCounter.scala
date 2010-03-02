7066869e95b58ce18a8e32fa3ed78c4bfc0df190
Justin <dell.justin@gmail.com>
2010-03-01 20:23:00 -0600

    4 mil... not 1

--- src/problem2/EvenFibCounter.scala
+++ src/problem2/EvenFibCounter.scala
@@ -4,9 +4,9 @@ object EvenFibCounter extends Application {
 	var sum = 0
 	var fibNumbers = List[Int](1, 0)
  
-	while (fibNumbers(0) < 1000000) {
+	while (fibNumbers(0) < 4000000) {
 	  fibNumbers = fibNumbers(0) + fibNumbers(1) :: fibNumbers
-	  if (fibNumbers(0) % 2 == 0 && fibNumbers(0) < 1000000)
+	  if (fibNumbers(0) % 2 == 0 && fibNumbers(0) < 4000000)
 		  sum += fibNumbers(0)
 	}
  
