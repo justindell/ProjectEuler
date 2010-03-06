3919ef9521c3dc7725b8ac1cba537b759559d4cc
Justin <dell.justin@gmail.com>
2010-03-06 15:34:30 -0600

    Problem 5 code

---
+++ src/problem5/EvenlyDivisible.scala
@@ -0,0 +1,16 @@
+package problem5
+
+class EvenlyDivisible {
+  def main(args: Array[String]) {
+    val runningLCM = (1L to 20L).reduceLeft(lcm(_,_))
+    println("Smallest is " + runningLCM)
+    
+  }
+
+  def lcm(one: Long, two: Long): Long = (one * two) / gcd(one, two)
+
+  def gcd(a: Long, b: Long): Long = {
+    if (b == 0) return a
+    return gcd(b, a - (b * (a/b)))
+  }
+}
