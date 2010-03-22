a028c9d7c49b5403c579c57b970fdc0fdc9f999d
Justin <dell.justin@gmail.com>
2010-03-21 20:20:52 -0500

    problem 10 in ruby

--- ruby/problem10.rb
+++ ruby/problem10.rb
@@ -1,3 +1,10 @@
-class Problem10
-  #Code here
-end
+require 'mathn.rb'
+
+sum = 0
+Prime.each() { |prime|
+  if (prime > 2000000)
+    puts "Sum is: " + sum.to_s
+    exit
+  end
+  sum += prime
+}
