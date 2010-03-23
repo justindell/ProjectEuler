e4000dbdf7a4dddee1c240f1cff9e9f143c4dd04
Justin <jdell@drw.com>
2010-03-23 12:38:41 -0500

    refactored to one line

--- ruby/problem7.rb
+++ ruby/problem7.rb
@@ -1,11 +1,3 @@
 require 'mathn.rb'
 
-x = 0
-puts "10001st Prime: "
-Prime.each(1000000){ |i|
-  x += 1
-  if (x == 10001)
-    puts i.to_s
-    break
-  end
-}
+puts "10001st Prime: " + Prime.take(10001).last.to_s
