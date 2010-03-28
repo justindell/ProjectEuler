0af450b94e02ac906f5360c92da245ad8b397d59
Justin <dell.justin@gmail.com>
2010-03-27 20:51:53 -0500

    made line smaller

--- ruby/problem10.rb
+++ ruby/problem10.rb
@@ -1,3 +1,3 @@
 require 'mathn.rb'
 
-puts "Sum is: " + Prime.take_while {|prime| prime < 2000000}.inject { |sum, n| sum + n}.to_s
+puts "Sum is: " + Prime.each(2000000).inject { |sum, n| sum + n}.to_s
