22c716a36088962c123c66cc7c8a962e4467890e
Justin <dell.justin@gmail.com>
2010-03-27 19:38:43 -0500

    refactor

--- ruby/problem9.rb
+++ ruby/problem9.rb
@@ -1,12 +1,10 @@
-class Problem9
-  (1..1000).each { |a|
-    aSquared = a**2  # So we dont calculate it over and over
-    (1..a).each { |b|
-      c = 1000 - a - b
-      if (c**2 == aSquared + b**2)
-        puts "a: " + a.to_s + " b: " + b.to_s + " c: " + c.to_s
-        puts "Product: " + (a*b*c).to_s
-      end
-    }
+(1..1000).each { |a|
+  a_squared = a**2  # So we dont calculate it over and over
+  (1..a).each { |b|
+    c = 1000 - a - b
+    if (c**2 == a_squared + b**2)
+      puts "Product: " + (a*b*c).to_s
+      exit 
+    end
   }
-end
+}
