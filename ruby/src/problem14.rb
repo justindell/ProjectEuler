57e1088a7addde83cc3f0923497b4048b561f1ea
Justin Dell <dell.justin@gmail.com>
2010-03-31 21:07:52 -0500

    problem 14 solution

---
+++ ruby/src/problem14.rb
@@ -0,0 +1,34 @@
+$num_items = [0]
+
+def get_num_items(num) 
+  if num == 1
+    return 1
+  end
+  if $num_items[num]
+    return 1 + $num_items[num]
+  end
+  if num % 2 == 0
+    return 1 + get_num_items(num / 2)
+  else
+    return 1 + get_num_items(3 * num + 1)
+  end
+end
+
+best = 0
+best_candidate = 0
+(1..1000000).each { |cand|
+  items = get_num_items(cand)
+  $num_items << items
+  if (items > best)
+    best = items
+    best_candidate = cand
+  end
+}
+
+#j = 0
+#$num_items.each{ |i| 
+#  puts j.to_s +  ": " + i.to_s
+#  j += 1
+#}
+
+puts best_candidate.to_s + " at " + best.to_s
