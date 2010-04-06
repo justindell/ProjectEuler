b133271dd5176021a3ad78e19dc29b096bed06a7
Justin Dell <dell.justin@gmail.com>
2010-04-06 16:57:15 -0500

    removed optimization that didnt work

--- ruby/src/problem13.rb
+++ ruby/src/problem13.rb
@@ -1,3 +1,3 @@
 numbers = IO.readlines("../data/problem13.txt")
 
-puts numbers.inject(0){|sum, line| sum + line.slice(0..10).to_i}.to_s.slice(0..9)
+puts numbers.inject(0){|sum, line| sum + line.to_i}.to_s.slice(0..9)
