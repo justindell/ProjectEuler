a291d3dab1e5fa20aee37cfb7f281ac66483e30e
Justin Dell <dell.justin@gmail.com>
2010-04-07 14:07:14 -0500

    refactor

--- ruby/src/problem15.rb
+++ ruby/src/problem15.rb
@@ -5,7 +5,6 @@ def num_paths(x,y)
   return 1 if (x == 1 && y == 0) || (x == 0 && y == 1)
   return $found_paths[x][y] if $found_paths[x][y]
   $found_paths[x][y] = num_paths(x-1,y) + num_paths(x,y-1)
-  return $found_paths[x][y]
 end
 
 puts num_paths(20,20)
