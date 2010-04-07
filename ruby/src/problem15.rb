b0fd1763b1661737bbd3a187b0fffba4609278fa
Justin Dell <dell.justin@gmail.com>
2010-04-07 13:59:56 -0500

    refactor

--- ruby/src/problem15.rb
+++ ruby/src/problem15.rb
@@ -1,10 +1,9 @@
 $found_paths = Array.new(21) { Array.new(21) }
 
 def num_paths(x,y)
-  return 0 if x == 0 && y == 0
-  return 0 if x < 0 || y < 0
-  return $found_paths[x][y] if $found_paths[x][y]
+  return 0 if (x == 0 && y == 0) || (x < 0 || y < 0)
   return 1 if (x == 1 && y == 0) || (x == 0 && y == 1)
+  return $found_paths[x][y] if $found_paths[x][y]
   $found_paths[x][y] = num_paths(x-1,y) + num_paths(x,y-1)
   return $found_paths[x][y]
 end
