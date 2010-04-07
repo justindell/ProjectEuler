805181d67165807228765fdb6399cb0b0730d68f
Justin Dell <dell.justin@gmail.com>
2010-04-07 15:32:17 -0500

    added space back

--- ruby/src/problem9.rb
+++ ruby/src/problem9.rb
@@ -1,7 +1,7 @@
 1000.times { |a|
   a_squared = a**2  # So we dont calculate it over and over
-  (1..a).each { |b|
+  (1..a).each do |b|
     c = 1000 - a - b
-    puts "Product: " + (a*b*c).to_s if (c**2 == a_squared + b**2)
-  }
+    puts "Product: #{(a*b*c)}" if (c**2 == a_squared + b**2)
+  end
 }
