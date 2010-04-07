b89f06ddeb713763d3d6e2310d9549592a641727
Justin Dell <dell.justin@gmail.com>
2010-04-07 14:44:02 -0500

    refactor to one line

--- ruby/src/problem20.rb
+++ ruby/src/problem20.rb
@@ -1,6 +1,1 @@
-def fact(n)
-  return 1 if n==0
-  return n * fact(n-1)
-end
-
-puts fact(100).to_s.split(//).inject(0){|sum,n| sum + n.to_i}
+puts (1..100).inject(1){|factorial,i| factorial * i}.to_s.split(//).inject(0){|sum,n| sum + n.to_i}
